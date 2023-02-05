4d0de18398367a42754e1998bdc1b3a21239562e
CarlosArte91 <carturo99@gmail.com>
2023-02-05 18:40:19 -0500

    finish commit

---
+++ src/hooks/use_ref/use_ref_exam_1.jsx
@@ -0,0 +1,8 @@
+/**
+ * En este ejemplo se practican los parametros que se pasan a un componente hijo
+ * y la ejecución de un método desde el hijo hacia el padre para actualizar el estado
+ * También se utiliza el
+ */
+function FatherComponent() {};
+
+export default FatherComponent;
